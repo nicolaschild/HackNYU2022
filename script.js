36cea4f2d7522cfb28afc81ed504d474fe1664ab
Nicolas Child <nickchild14@gmail.com>
2022-02-25 18:47:19 -0700

    Skeleton for website

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    console.log(document.getElementById("header"))
+
+
+
+}
+
+/*
+    
+
+    class transportation(CarbonFootPrint):
+        
+        def go(self, kilometers, type):
+            (dis + fuel use) * emission_factor
+    
+        
+
+*/
+
