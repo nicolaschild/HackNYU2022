8f94727707e51fd0ae0b5c311d86d4c8212208d2
Ty Klabacka <86436972+Tklaback@users.noreply.github.com>
2022-02-26 18:35:11 -0700

    added new features, all tests passing

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-    console.log(document.getElementById("header"))
     //Adding text to the text boxes
     //TODO - make it disapear on click event
     //Need to call the load_text any time the user's focus leaves the box so it shows back up
     load_text();
+    document.getElementById("travelInput").addEventListener("click", clearTravelInput);
+    document.getElementById("plasticInput").addEventListener("click", clearPlasticInput);
+    document.getElementById("electricalInput").addEventListener("click", clearElectricalInput);
 }
 
 function clearTravelInput() {
     //Function to clear the text box for CInput
     document.getElementById("travelInput").value = "";
+ }
+
+ function clearPlasticInput() {
+    //Function to clear the text box for CInput
+    document.getElementById("plasticInput").value = "";
+ }
+
+ function clearElectricalInput() {
+    //Function to clear the text box for CInput
+    document.getElementById("electricalInput").value = "";
  }
 
 
@@ -29,39 +41,49 @@ function update(amount) {
     element.style.width = width + '%'; 
     }
 
-
-function Plane(distance) {
-    this.emissions = 69.25;
-    this.distance = distance;
-    this.fuel_usage = 61.55;
+class Plane {
+     constructor(distance) {
+        this.distance = distance;
+        this.fuel_usage = 61.55;
+    }
 }
 
-function Car(distance){
-    this.emissions = 70.22;  //emission factor
-    this.distance = distance; //distance in miles
-    this.fuel_usage = 25.4; //miles per gallon
+class Car{
+    constructor(distance){
+        this.distance = distance; //distance in miles
+        this.fuel_usage = 25.4; //miles per gallon
+    }
 }
+
 
 function go(distance, type){
     type = type.toLowerCase()
+    var myObject;
     switch(type){
         case "plane": 
-            var type = new Plane(distance)
+            myObject = new Plane(distance)
             break;
         case "car":
-            var type = new Car(distance);
+            myObject = new Car(distance);
             break;
         default:
             alert("We don't have that, sorry")
     }
-    return (type.distance + type.fuel_usage) * type.emissions;
+    load_text();
+    return (myObject.distance / myObject.fuel_usage) * 20;
 }
 
 function callGo(){
-    alert("hello");
-    let miles = int(document.getElementById("travelInput").value);
+    let miles = parseInt(document.getElementById("travelInput").value);
     let method = document.getElementById("travelMethod").value;
-    
-    // go(miles, method);
-    // update(amount);
+    //Need to check if miles is NaN, then we can throw an error and not call the function
+    error = document.getElementById("error");
+    if (isNaN(miles)) {
+       error.innerHTML = "Please enter a number!";
+    }
+    else {
+       error.innerHTML = "";
+       var amount = .1 * go(miles, method);
+       update(amount);
+    }
 }
